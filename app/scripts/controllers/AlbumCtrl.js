820bf5b82801bb408e328cc59b106e0623fdfe56
Sergio Guerrero <sguerrero1971@gmail.com>
2017-01-20 13:12:17 -0500

    assignment-7_services_2

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -2,11 +2,12 @@
  * Created by sguerrero on 1/18/17.
  */
 (function() {
-    function AlbumCtrl(Fixtures) {
+    function AlbumCtrl(Fixtures, SongPlayer) {
         this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
     }
 
     angular
         .module('blocJams')
-        .controller('AlbumCtrl',['Fixtures', AlbumCtrl]);
+        .controller('AlbumCtrl',['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
