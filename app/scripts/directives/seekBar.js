a5cf63af2d69cbf146d58654ada6956e4282460d
Sergio Guerrero <sguerrero1971@gmail.com>
2017-01-24 20:34:26 -0500

    checkpoint-10_directives_2

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -16,12 +16,22 @@
             templateUrl: '/templates/directives/seek_bar.html',
             replace: true,
             restrict: 'E',
-            scope: { },
+            scope: {
+                onChange: '&'
+            },
             link: function(scope, element, attributes) {
                 scope.value = 0;
                 scope.max = 100;
 
                 var seekBar = $(element);
+
+                attributes.$observe('value', function(newValue) {
+                    scope.value = newValue;
+                });
+
+                attributes.$observe('max', function(newValue) {
+                    scope.max = newValue;
+                });
 
                 var percentString = function () {
                     var value = scope.value;
@@ -37,6 +47,7 @@
                 scope.onClickSeekBar = function(event) {
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
+                    notifyOnChange(scope.value);
                 };
 
                 scope.trackThumb = function() {
@@ -44,6 +55,7 @@
                         var percent = calculatePercent(seekBar, event);
                         scope.$apply(function() {
                             scope.value = percent * scope.max;
+                            notifyOnChange(scope.value);
                         });
                     });
 
@@ -55,6 +67,13 @@
 
                 scope.thumbStyle = function() {
                     return {left: percentString()};
+                };
+
+
+                var notifyOnChange = function(newValue) {
+                    if (typeof scope.onChange === 'function') {
+                        scope.onChange({value: newValue});
+                    }
                 };
             }
         };
